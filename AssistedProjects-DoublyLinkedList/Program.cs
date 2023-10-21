afbabb8fe17f0b8ff0c474c4b87dc7dec9ccfbc7
Kishore Kaladasi <kishorekaladasi2001@gmail.com>
2023-10-21 13:40:17 +0530

    Add project files.

---
+++ AssistedProjects-DoublyLinkedList/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssistedProjects_DoublyLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        public static void runApp()
+        {
+            DoublyLinkedList list = new DoublyLinkedList();
+            Node n = new Node();
+            n.Data = "root";
+            list.add(n);
+            for (int i = 0; i < 10; i++)
+            {
+                Node node = new Node();
+                node.Data = Convert.ToString(i);
+                list.add(node);
+            }
+
+            Console.WriteLine("length of doubly linked list=" + list.getLengthOfList());
+            Console.WriteLine("Traversing forward..");
+            n = list.getRoot();
+            while (n != null)
+            {
+                Console.WriteLine(n.Data);
+                n = list.getNext();
+            }
+
+
+            Console.WriteLine("Traversing backwards..");
+            n = list.getPrev();
+            while (n != null)
+            {
+                Console.WriteLine(n.Data);
+                n = list.getPrev();
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
